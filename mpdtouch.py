72af2d5ba5ea93e41018bd2508f838edea648684
Gerald Schneider <gerald@schneidr.de>
2016-02-09 17:50:09 +0000

    added basic mpd status and control

--- mpdtouch.py
+++ mpdtouch.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python
 from gi.repository import Gtk
+from mpd import MPDClient
 
 # http://helloraspberrypi.blogspot.de/2013/12/install-and-program-with-gtk-30-in.html
 # https://oli4444.wordpress.com/2015/11/22/from-power-on-to-gtk-gui-usable-as-fast-as-possible/
-# sudo aptitude install python-gi gir1.2-gtk-3.0 libgtk-3-dev
+# sudo aptitude install python-gi gir1.2-gtk-3.0 libgtk-3-dev python-mpd
 
 class MyWindow(Gtk.Window):
+
+    mpdHost = "192.168.23.104"
+    mpdPort = 6600
+    mpdPassword = ""
 
     def __init__(self):
         Gtk.Window.__init__(self, title="MPD Touch")
@@ -67,7 +72,6 @@ class MyWindow(Gtk.Window):
         self.nowPlayingPage.add(self.nowPlayingProgress)
 
         # control buttons
-
         self.nowPlayingControls = Gtk.Box(spacing=6)
         self.controlButtonPrevious = Gtk.Button()
         self.controlButtonPrevious.add(
@@ -76,7 +80,7 @@ class MyWindow(Gtk.Window):
                 Gtk.IconSize.BUTTON
             )
         )
-        #self.controlButtonPrevious.connect("clicked", self.on_controlButtonPrevious_clicked)
+        self.controlButtonPrevious.connect("clicked", self.on_controlButtonPrevious_clicked)
         self.nowPlayingControls.pack_start(self.controlButtonPrevious, True, True, 0)
 
         self.controlButtonPlayPause = Gtk.Button()
@@ -86,7 +90,7 @@ class MyWindow(Gtk.Window):
                 Gtk.IconSize.BUTTON
             )
         )
-        #self.controlButtonPlayPause.connect("clicked", self.on_controlButtonPlayPause_clicked)
+        self.controlButtonPlayPause.connect("clicked", self.on_controlButtonPlayPause_clicked)
         self.nowPlayingControls.pack_start(self.controlButtonPlayPause, True, True, 0)
 
         self.controlButtonStop = Gtk.Button()
@@ -96,7 +100,7 @@ class MyWindow(Gtk.Window):
                 Gtk.IconSize.BUTTON
             )
         )
-        #self.controlButtonStop.connect("clicked", self.on_controlButtonStop_clicked)
+        self.controlButtonStop.connect("clicked", self.on_controlButtonStop_clicked)
         self.nowPlayingControls.pack_start(self.controlButtonStop, True, True, 0)
 
         self.controlButtonNext = Gtk.Button()
@@ -106,7 +110,7 @@ class MyWindow(Gtk.Window):
                 Gtk.IconSize.BUTTON
             )
         )
-        #self.controlButtonNext.connect("clicked", self.on_controlButtonNext_clicked)
+        self.controlButtonNext.connect("clicked", self.on_controlButtonNext_clicked)
         self.nowPlayingControls.pack_start(self.controlButtonNext, True, True, 0)
 
         self.controlButtonRepeat = Gtk.ToggleButton()
@@ -163,14 +167,14 @@ class MyWindow(Gtk.Window):
         self.settingsPage = Gtk.Box()
         self.settingsPage.set_border_width(10)
 
-        self.editWindow = Gtk.ScrolledWindow()
-        self.editWindow.set_policy(Gtk.PolicyType.NEVER, Gtk.PolicyType.AUTOMATIC)
+        self.settingsScrollWindow = Gtk.ScrolledWindow()
+        self.settingsScrollWindow.set_policy(Gtk.PolicyType.NEVER, Gtk.PolicyType.AUTOMATIC)
 
         self.settingsListBox = Gtk.ListBox()
         self.settingsListBox.set_selection_mode(Gtk.SelectionMode.NONE)
-        self.editWindow.add(self.settingsListBox)
+        self.settingsScrollWindow.add(self.settingsListBox)
 
-        self.settingsPage.add(self.editWindow)
+        self.settingsPage.add(self.settingsScrollWindow)
         self.notebook.append_page(
             self.settingsPage,
             Gtk.Image.new_from_icon_name(
@@ -179,7 +183,84 @@ class MyWindow(Gtk.Window):
             )
         )
 
+        self.client = MPDClient()
+        self.client.timeout = 10               
+        self.client.idletimeout = None         
+        self.client.connect(self.mpdHost, self.mpdPort) 
+
+        self.updateMpd()
+
+        self.connect("delete-event", self.mainQuit)
+
+    def on_controlButtonPlayPause_clicked(self, b):
+        self.client.play()
+        self.updateMpd()
+
+    def on_controlButtonStop_clicked(self, b):
+        self.client.stop()
+        self.updateMpd()
+
+    def on_controlButtonPrevious_clicked(self, b):
+        self.client.previous()
+        self.updateMpd()
+
+    def on_controlButtonNext_clicked(self, b):
+        self.client.next()
+        self.updateMpd()
+
+    def format_time(self, input):
+        nums = input.split(':')
+        seconds = int(nums[0])
+        result = []
+        hours = seconds / 3600
+        seconds = seconds % 3600
+        minutes = seconds / 60
+        seconds = seconds % 60
+        if hours > 0:
+            result.append(str(hours))
+        if minutes > 0:
+            result.append(str(minutes))
+        result.append(str(seconds))
+        return ':'.join(result)
+
+    def mainQuit(self, b, c):
+        self.client.close()
+        self.client.disconnect()
+        Gtk.main_quit(self, b, c)
+
+    def updateMpd(self):
+
+        status = self.client.status()
+        # {'songid': '1', 'playlistlength': '1', 'playlist': '132', 'repeat': '0', 'consume': '0', 
+        # 'mixrampdb': '0.000000', 'random': '1', 'state': 'play', 'elapsed': '99.186', 'volume': '75', 
+        # 'single': '0', 'time': '99:0', 'song': '0', 'audio': '44100:24:2', 'bitrate': '128'}
+
+        if status['state'] == "play":
+            self.controlButtonPlayPause.set_image(
+                Gtk.Image.new_from_icon_name(
+                    "media-playback-pause",
+                    Gtk.IconSize.BUTTON
+                )
+            )
+        else:
+            self.controlButtonPlayPause.set_image(
+                Gtk.Image.new_from_icon_name(
+                    "media-playback-start",
+                    Gtk.IconSize.BUTTON
+                )
+            )
+        self.controlButtonRepeat.set_active( status['repeat'] == '1' )
+        self.controlButtonShuffle.set_active( status['random'] == '1' )
+
+        currentSong = self.client.currentsong()
+        # {'id': '1', 'pos': '0', 'name': 'hr3', 
+        # 'file': 'http://hr-mp3-m-h3.akacast.akamaistream.net/7/785/142133/v1/gnl.akacast.akamaistream.net/hr-mp3-m-h3', 
+        # 'title': 'Bloodhound Gang - The bad touch'}
+        self.timeLabel.set_markup(self.format_time(status["time"]))
+        self.nowPlayingTrackTitle.set_markup("<b>" + currentSong["title"] + "</b>")
+        self.nowPlayingTrackArtist.set_markup(currentSong["name"])
+
+
 win = MyWindow()
-win.connect("delete-event", Gtk.main_quit)
 win.show_all()
 Gtk.main()
